e20a8f6ac0a24ab32aaf2c35e250384061e7850c
PILLA NAGA ADINARAYANA <22h41a0444adinarayana@gmail.com>
2026-05-27 22:36:54 +0530

    Created main.ino

---
+++ code/main.ino
@@ -0,0 +1,67 @@
+#define S0 4
+#define S1 5
+#define S2 6
+#define S3 7
+#define sensorOut 8
+
+int redFrequency = 0;
+int greenFrequency = 0;
+int blueFrequency = 0;
+
+void setup() {
+
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  pinMode(sensorOut, INPUT);
+
+  digitalWrite(S0, HIGH);
+  digitalWrite(S1, LOW);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  // Read RED
+  digitalWrite(S2, LOW);
+  digitalWrite(S3, LOW);
+  redFrequency = pulseIn(sensorOut, LOW);
+
+  // Read GREEN
+  digitalWrite(S2, HIGH);
+  digitalWrite(S3, HIGH);
+  greenFrequency = pulseIn(sensorOut, LOW);
+
+  // Read BLUE
+  digitalWrite(S2, LOW);
+  digitalWrite(S3, HIGH);
+  blueFrequency = pulseIn(sensorOut, LOW);
+
+  Serial.print("R = ");
+  Serial.print(redFrequency);
+  Serial.print("  G = ");
+  Serial.print(greenFrequency);
+  Serial.print("  B = ");
+  Serial.println(blueFrequency);
+
+  // Simple Colour Detection Logic
+  if (redFrequency < greenFrequency && redFrequency < blueFrequency) {
+    Serial.println("Detected Colour: RED");
+  }
+
+  else if (greenFrequency < redFrequency && greenFrequency < blueFrequency) {
+    Serial.println("Detected Colour: GREEN");
+  }
+
+  else if (blueFrequency < redFrequency && blueFrequency < greenFrequency) {
+    Serial.println("Detected Colour: BLUE");
+  }
+
+  else {
+    Serial.println("Colour Not Identified");
+  }
+
+  delay(1000);
+}
